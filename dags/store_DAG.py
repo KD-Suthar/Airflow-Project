cc4b957f31993a736f10796ea3cb199d59ba16d1
Kuldeep <111103707+KD-Suthar@users.noreply.github.com>
2022-12-17 16:32:50 +0530

    Update store_DAG.py

--- dags/store_DAG.py
+++ dags/store_DAG.py
@@ -18,26 +18,57 @@ default_args = {
 
 with DAG('store_dag_final',default_args=default_args,schedule_interval='@daily', template_searchpath=['/usr/local/airflow/sql_files'], catchup=True) as dag:
 
-    t1=BashOperator(task_id='check_file_exists', bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv', retries=2, retry_delay=timedelta(seconds=15))
+    t1=BashOperator(
+        task_id='check_file_exists', 
+        bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv', 
+        retries=2, 
+        retry_delay=timedelta(seconds=15)
+    )
 
-    t2 = PythonOperator(task_id='clean_raw_csv', python_callable=data_cleaner)
+    t2 = PythonOperator(
+        task_id='clean_raw_csv', 
+        python_callable=data_cleaner
+    )
 
-    t3 = MySqlOperator(task_id='create_mysql_table', mysql_conn_id="mysql_conn", sql="create_table.sql")
+    t3 = MySqlOperator(
+        task_id='create_mysql_table', 
+        mysql_conn_id="mysql_conn", 
+        sql="create_table.sql"
+    )
 
-    t4 = MySqlOperator(task_id='insert_into_table', mysql_conn_id="mysql_conn", sql="insert_into_table.sql")
+    t4 = MySqlOperator(
+        task_id='insert_into_table', 
+        mysql_conn_id="mysql_conn", 
+        sql="insert_into_table.sql"
+    )
 
-    t5 = MySqlOperator(task_id='select_from_table', mysql_conn_id="mysql_conn", sql="select_from_table.sql")
+    t5 = MySqlOperator(
+        task_id='select_from_table',
+        mysql_conn_id="mysql_conn", 
+        sql="select_from_table.sql"
+    )
 
-    t6 = BashOperator(task_id='move_file1', bash_command='cat ~/store_files_airflow/location_wise_profit.csv && mv ~/store_files_airflow/location_wise_profit.csv ~/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date)
+    t6 = BashOperator(
+        task_id='move_file1', 
+        bash_command='cat ~/store_files_airflow/location_wise_profit.csv && mv ~/store_files_airflow/location_wise_profit.csv ~/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date
+    )
 
-    t7 = BashOperator(task_id='move_file2', bash_command='cat ~/store_files_airflow/store_wise_profit.csv && mv ~/store_files_airflow/store_wise_profit.csv ~/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date)
+    t7 = BashOperator(
+        task_id='move_file2', 
+        bash_command='cat ~/store_files_airflow/store_wise_profit.csv && mv ~/store_files_airflow/store_wise_profit.csv ~/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date
+    )
 
-    t8 = EmailOperator(task_id='send_email',
+    t8 = EmailOperator(
+        task_id='send_email',
         to='example@example.com',
         subject='Daily report generated',
         html_content=""" <h1>Congratulations! Your store reports are ready.</h1> """,
-        files=['/usr/local/airflow/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date, '/usr/local/airflow/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date])
+        files=['/usr/local/airflow/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date, '/usr/local/airflow/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date]
+    )
 
-    t9 = BashOperator(task_id='rename_raw', bash_command='mv ~/store_files_airflow/raw_store_transactions.csv ~/store_files_airflow/raw_store_transactions_%s.csv' % yesterday_date)
+    t9 = BashOperator(
+        task_id='rename_raw', 
+        bash_command='mv ~/store_files_airflow/raw_store_transactions.csv ~/store_files_airflow/raw_store_transactions_%s.csv' % yesterday_date
+    )
 
     t1 >> t2 >> t3 >> t4 >> t5 >> [t6,t7] >> t8 >> t9
